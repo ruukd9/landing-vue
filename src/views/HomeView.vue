c314c0f7d21ebe8c036db50662d5c48ed62603fe
Luca Bassi <luca96bass@gmail.com>
2025-01-23 12:16:15 +0100

    tailwind cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </main>
 </template>
